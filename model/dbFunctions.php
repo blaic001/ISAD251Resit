2349b465d698f8d39e867d0c0f3e5d7d108d158a
blaic001 <55986457+blaic001@users.noreply.github.com>
2020-08-24 00:24:03 +0100

    BuildUser and GetUserData Wrote
    Wrote them but not fully tested yet. Will do tomorrow.

--- model/dbFunctions.php
+++ model/dbFunctions.php
@@ -23,4 +23,60 @@ function getUser($firstName){
     $userId = getUserId($firstName);
     $appointmentData = getAppointmentData($userId);
     $deadlineData = getDeadlineData($userId);
+
+    $user = buildUser($userData, $appointmentData, $deadlineData);
+    return $user;
+}
+
+function buildUser($userData, $appointmentData, $deadlineData){
+    $appointmentDataArray = array();
+    $deadlineDataArray = array();
+
+    for ($i = 0; $i < count($userData); ++$i){
+        $userId = $userData[$i]["UserId"];
+        $firstName = $userData[$i]["firstName"];
+        $lastName = $userData[$i]["lastName"];
+        $age = $userData[$i]["age"];
+    }
+
+    for ($i = 0; $i < count($appointmentData); ++$i){
+        $appId = $appointmentData[$i]["userId"];
+        $userId = $appointmentData[$i]["appId"];
+        $appDT = $appointmentData[$i]["appDT"];
+        $appNotes = $appointmentData[$i]["appNotes"];
+        $appDesc = $appointmentData[$i]["appDesc"];
+
+        $appDataPoint = new appData($appId, $userId, $appDT, $appNotes, $appDesc);
+
+        array_push($appointmentDataArray, $appDataPoint);
+    }
+
+    for ($i = 0; $i < count($deadlineData); ++$i){
+        $deadlineId = $deadlineData[$i]["deadlineId"];
+        $userId = $deadlineData[$i]["userId"];
+        $deadlineDT = $deadlineData[$i]["deadlineDT"];
+        $deadlineDesc = $deadlineData[$i]["deadlineDesc"];
+        $deadlineCheck = $deadlineData[$i]["deadlineCheck"];
+
+        $deadlineDataPoint = new deadData($deadlineId, $userId, $deadlineDT, $deadlineDesc, $deadlineCheck);
+
+        array_push($deadlineDataArray, $deadlineDataPoint);
+
+    }
+
+    return $user = new userData($userId, $firstName, $lastName, $age, $appointmentDataArray, $deadlineDataArray);
+
+}
+
+function getUserData($firstName){
+    $statement = getConnect()->prepare("CALL GetUserId('" . $firstName . "')");
+    $statement->execute();
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    for ($i = 0; $i < count($data); ++$i) {
+        $userId = $data[$i]["userId"];
+        return $userId;
+    }
+
+    return 1;
 }
